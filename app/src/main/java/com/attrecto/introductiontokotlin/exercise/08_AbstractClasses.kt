dae1b8bfa46f19e0da24e56431e5e8f653b9aad3
balazsnemeth <balazs.nemeth@attrecto.com>
2021-10-06 14:03:31 +0200

    abstract exercises

---
+++ app/src/main/java/com/attrecto/introductiontokotlin/exercise/08_AbstractClasses.kt
@@ -0,0 +1,5 @@
+package com.attrecto.introductiontokotlin.exercise
+
+// Alakítsd át az öröklődéses feladatot, hogy az "Animal" osztály és a say() absztrakt legyen
+// Használt az előző feladat kódját, vagy rakd bele az osztályokat a main() fv-be különben
+// osztály újra definilása miatt nem fordul
---
+++ app/src/main/java/com/attrecto/introductiontokotlin/solution/08_AbstractClasses.kt
@@ -0,0 +1,28 @@
+package com.attrecto.introductiontokotlin.solution
+
+// Alakítsd át az öröklődéses feladatot, hogy az "Animal" osztály és a say() absztrakt legyen
+// Használt az előző feladat kódját, vagy rakd bele az osztályokat a main() fv-be különben
+// osztály újra definilása miatt nem fordul
+
+fun main() {
+    abstract class Animal{
+        abstract fun say()
+    }
+
+    class Fox : Animal() {
+        override fun say(){
+            println("Ring-ding-ding-ding-dingeringeding!")
+        }
+    }
+
+    class Cat : Animal() {
+        override fun say(){
+            println("Woof!")
+        }
+    }
+
+    val f = Fox()
+    f.say()
+    val c = Cat()
+    c.say()
+}
